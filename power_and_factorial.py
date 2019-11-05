d271d6069102799fdc20c2ccbc37d166dd6be30c
Art Raj Tiwari <artrajtiwari@gmail.com>
2019-11-05 20:26:13 +0545

    update power func

--- power_and_factorial.py
+++ power_and_factorial.py
@@ -1,19 +1,20 @@
 # Todo : find the power of the numbers
-def power(n,p):
-    if p == 0 :
+def power(n, p):
+    if p == 0:
         # ! we return 1 as for the process of multiplying it with 2**0
         return 1
     else:
-        return n * power(n,p-1)    
+        return n * power(n, p-1)
 
-print('the power 2 to 3rd is {}'.format(power(2,3)) )       
 
+print('the power 2 to 3rd is {}'.format(power(2, 3)))
 
 
 def factorial(n):
     if n == 1:
         return 1
     else:
-        return n * factorial(n-1)    
+        return n * factorial(n-1)
 
-print('the factorial of {} is {}'.format(5,factorial(5))  )      
+
+print('the factorial of {} is {}'.format(5, factorial(5)))
